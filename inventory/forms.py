9efe8acf1e5c59f2e213fba03d98e33a686c7db0
hasalioma3 <hassan.omar@naivas.co.ke>
2023-01-04 17:54:28 +0300

    Add

---
+++ inventory/forms.py
@@ -0,0 +1,8 @@
+Add from django.forms import ModelForm
+from .models import Inventory
+
+
+class AddInventoryForm(ModelForm):
+    class Meta:
+        model = Inventory
+        fields = ['name', 'cost_per_item', 'quantity_in_stock', 'quantity_sold']
